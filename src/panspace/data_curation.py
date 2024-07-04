99a8536e3f78d968198a041da810d2320e43375a
jorge <jorgeavilacartes@gmail.com>
2024-07-04 15:36:29 +0200

    update data curation

--- src/panspace/data_curation.py
+++ src/panspace/data_curation.py
@@ -78,9 +78,9 @@ def outlier_detection(
 def preds_confident_learning(
     # paths
     path_train_embeddings: Annotated[Path, typer.Option("--path-train-embeddings", help="path to .npy file with embeddings.")],
-    path_train_labels: Annotated[Path, typer.Option("--path-train-labels", help="path to .json file with labels.")],
+    path_train_labels: Annotated[Path, typer.Option("--path-train-labels", help="path to txt file with path and labels.")],
     path_test_embeddings: Annotated[Path, typer.Option("--path-test-embeddings", help="path to .npy file with embeddings.")],
-    path_test_labels: Annotated[Path, typer.Option("--path-train-embeddings", help="path to .npy file with embeddings.")],
+    path_test_labels: Annotated[Path, typer.Option("--path-train-embeddings", help="path to txt file with path and labels.")],
     outdir: Annotated[Path, typer.Option("--outdir","-o", help="output directory to save results")],
     order_labels: Annotated[Path, typer.Option("--order-labels","-ol", help="txt file with all unique labels, one label per row")] = None,
     ):
@@ -96,20 +96,25 @@ def preds_confident_learning(
     outdir.mkdir(exist_ok=True, parents=True)
 
     X_train = np.load(path_train_embeddings) 
+    y_train = []
     with open(path_train_labels) as fp:
-        y_train = json.load(fp)
-        y_train = pd.Series(y_train, name="label") 
+        for line in fp.readlines():
+            path, label = line.split("\t")[-1]
+            y_train.append(label)
 
     if order_labels is None:
-        unique_labels = y_train.unique()
+        unique_labels = list(set(y_train))
         unique_labels.sort()
         dict_labels = {label: idx for idx, label in enumerate(unique_labels)}
     else:
         dict_labels = {label: idx for idx, label in enumerate(unique_labels)}
+    
     X_test = np.load(path_test_embeddings) 
+    y_test = []
     with open(path_test_labels) as fp:
-        y_test = json.load(fp)
-        y_test = pd.Series(y_test, name="label")
+        for line in fp.readlines():
+            path, label = line.split("\t")[-1]
+            y_test.append(label)
 
     labels_test = np.array([dict_labels[label] for label in y_test])
 
@@ -118,8 +123,6 @@ def preds_confident_learning(
     pred_probs = clf.predict_proba(X_test)
     np.save(file=outdir.joinpath("pred_probs.npy"), arr=pred_probs)
     np.save(file=outdir.joinpath("labels.npy"), arr=labels_test)
-
-    # labels_test.to_csv(outdir.joinpath("labels.csv"))
 
 
 @app.command("utils-join-npy")
