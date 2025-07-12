cdf23c9930ed8390ccf8873d44a81024d2d856cb
Jorge <jorgeavilacartes@gmail.com>
2025-07-12 19:48:30 +0200

    fix percentile, between 0 and 100

--- src/panspace/dataclasses_cli/preprocessing.py
+++ src/panspace/dataclasses_cli/preprocessing.py
@@ -4,5 +4,4 @@ class Preprocessing(str,Enum):
     Distribution="distribution"
     ScaleZeroOne="scale_zero_one"
     Clip="clip"
-    # ScaleZeroOne_Clip="scale_zero_one_clip"
     Clip_ScaleZeroOne="clip_scale_zero_one"
